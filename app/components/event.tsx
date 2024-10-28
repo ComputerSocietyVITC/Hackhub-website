78255182b89af86612049aea9b30669d4262b349
Shriansh006 <shriansh.tripathee06@gmail.com>
2024-10-29 00:49:56 +0530

    add: about content

--- app/components/event.tsx
+++ app/components/event.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 
 const Event = () => {
   return (
+<<<<<<< Updated upstream
     <div className="bg-cover bg-center h-screen w-screen flex items-center relative justify-center">
        <div
         className="bg-center bg-no-repeat bg-contain h-[2vh] w-[60vw] absolute overflow-auto"
@@ -14,6 +15,23 @@ const Event = () => {
           transform: 'translate(-50%, -50%)'
         }}
       />
+=======
+    <div className="bg-cover bg-center h-screen w-screen flex items-center justify-center">
+      <div className="min-h-screen w-full flex justify-center">
+        <div className="mt-12 w-full px-5">
+          <div className="flex justify-between items-center px-12">
+            <p className="text-white text-4xl font-bold">OTHER IEEE FLAGSHIP EVENTS</p>
+            <div className="flex-1 h-[1px] bg-white mx-4"></div>
+            <p className="text-white text-base font-bold ">02</p>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 mt-20 px-32">
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
+        </div>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 };
