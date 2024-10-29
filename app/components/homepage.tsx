1f2be63cc63ab918ed895b7f3728b8365734c03b
Shriansh006 <shriansh.tripathee06@gmail.com>
2024-10-30 00:53:08 +0530

    Update homepage.tsx

--- app/components/homepage.tsx
+++ app/components/homepage.tsx
@@ -7,8 +7,7 @@ import { Weight } from 'lucide-react';
 const Homepage: React.FC = () => {
   return (
    <div className="flex flex-col items-center justify-center h-screen w-screen relative">
-      {/* Ieee and compsoc text */}
-      <div
+      {/*<div
         className="absolute text-white text-8xl  font-light italic hidden md:block"
         style={{
           top: '29%', // Positioning above the logo
@@ -18,11 +17,7 @@ const Homepage: React.FC = () => {
       >
        IEEE
       </div>
-      <Image src='/images/compsoc.webp' alt="Background Track" width={90} height={90} className="absolute"
-       style={{
-        top:'2%' ,
-        left:'3%'
-       }} ></Image>
+      
       <div
         className="absolute  text-white text-5xl  font-extralight italic hidden md:block"
         style={{
@@ -32,13 +27,18 @@ const Homepage: React.FC = () => {
         }}
       >
        CompSoc
-      </div>
+      </div> */}
+      <Image src='/images/compsoc.webp' alt="Background Track" width={90} height={90} className="absolute"
+       style={{
+        top:'2%' ,
+        left:'3%'
+       }} ></Image>
       {/* Hackhub Logo */}
       <div
-        className="bg-center bg-no-repeat bg-contain h-[50vh] w-[50vw] absolute homepage-logo"
+        className="bg-center bg-no-repeat bg-contain h-[10vh] w-[50vw] absolute homepage-logo"
         style={{
           backgroundImage: 'url(images/Hackhub_logo.svg)',
-          top: '50%', // Base position; this will be overridden by media queries
+          top: '50%', 
           left: '50%',
           transform: 'translate(-50%, -50%)'
         }}
@@ -46,20 +46,17 @@ const Homepage: React.FC = () => {
 
       {/* 2025 Logo */}
       <div
-        className="bg-no-repeat bg-contain h-[10vh] w-[10vw] absolute homepage-2025-logo"
+        className="bg-no-repeat bg-contain h-[10vh] w-[10vw] top-[50%] left-[10%]  relative homepage-2025-logo transform -translate-x-1/2 -translate-y-1/2 md:top-[20%] md:-left-[5%]"
         style={{
           backgroundImage: 'url(images/2025.svg)',
-          top: '65%', // Base position; this will be overridden by media queries
-          left: '30%',
-          transform: 'translate(-50%, -50%)'
         }}
       />
        <div>
-       <div
+       {/*<div
         className="absolute text-white text-sm font-Archivo Narrow line-clamp-2 hidden md:block"
         style={{ width: '13%', height: '10%', top: '60%', left: '62%', gap: '0px', opacity: '1', fontSize: '90%' }}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </div>
+      </div>*/}
       
       </div>
     </div>
