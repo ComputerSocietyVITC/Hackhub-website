ee58e8fb0f43a0ee888bd07f4576cf112b170231
Shivam Kumar <savagedeveloper795@gmail.com>
2024-10-30 11:58:43 +0530

    Better breakpoints handling

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,7 +1,8 @@
+
 const Card = () => 
-<div className="min-h-[450px] rounded-[22px] col-span-4 bg-white/10 group overflow-hidden  last:justify-self-center last:sm:col-start-3 last:sm:col-end-7 last:lg:col-start-9 last:lg:col-end-13">
-    <img src="https://images.unsplash.com/photo-1729829471656-8488135a99ec" alt="" className="transition-all ease-in duration-150 h-full w-full object-cover saturate-0 group-hover:saturate-100" />
-</div>
-
-
-export default Card;
+    <div className="min-h-[350px] max-h-[450px] rounded-[22px] col-span-1 bg-white/10 group overflow-hidden sm:col-span-2 md:col-span-1 last:sm:col-start-2 last:sm:col-end-4 md:last:col-start-3 md:last:col-end-4">
+        <img src="https://images.unsplash.com/photo-1729829471656-8488135a99ec" alt="" className="transition-all ease-in duration-150 h-full w-full object-cover md:saturate-0 md:group-hover:saturate-100" />
+    </div>
+    
+    
+    export default Card;
