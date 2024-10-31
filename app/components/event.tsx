4955b887756ba56db1902b02af6952c9f3d7408e
Shivam Kumar <savagedeveloper795@gmail.com>
2024-10-31 14:20:27 +0530

    card image+desc+button and tablet comp

--- app/components/event.tsx
+++ app/components/event.tsx
@@ -12,15 +12,16 @@ const Event = () => {
             <div className="flex-1 h-[1px] bg-white mx-4"></div>
             <p className="text-white text-base font-bold ">02</p>
           </div>
-          <div className="grid container mx-auto py-12 md:py-20 lg:py-24 grid-cols-1 sm:grid-cols-4 md:grid-cols-3 gap-8 lg:gap-12 xl:gap-16 px-6 sm:px-12 md:px-24 xl:px-44">
-            <Card/>
-            <Card/>
-            <Card/>
+          <div className="grid container mx-auto py-12 md:py-20 lg:py-24 xl:py-36 grid-cols-1 sm:grid-cols-4 lg:grid-cols-3 gap-8 lg:gap-12 xl:gap-16 px-6 sm:px-12 md:px-24 xl:px-44">
+            <Card imgUrl='/images/paradox.png' alt="XYZ" description="Paradox24 is a competitive Capture The Flag (CTF) event organized by the IEEE Computer Society at VITC, designed to challenge participants across multiple tracks, including Reverse Engineering, Cryptography, Forensics, and OSINT."/>
+            <Card imgUrl='/images/bitwars.png' alt="XYZ" description="BitWars is an annual coding competition hosted by the IEEE Computer Society at VITC, where participants showcase their programming and problem-solving skills. The event includes challenges in algorithms, data structures, and software development, promoting innovation and collaboration among aspiring developers."/>
+            <Card imgUrl='https://images.unsplash.com/photo-1729829471656-8488135a99ec' alt="XYZ" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi tempore ab a nihil sint blanditiis deserunt saepe molestiae libero praesentium? Cupiditate recusandae repellat quaerat id iusto accusamus dolore laboriosam eum!"/>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default Event;
--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,8 +1,29 @@
+import React from 'react';
 
-const Card = () => 
-    <div className="min-h-[350px] max-h-[450px] rounded-[22px] col-span-1 bg-white/10 group overflow-hidden sm:col-span-2 md:col-span-1 last:sm:col-start-2 last:sm:col-end-4 md:last:col-start-3 md:last:col-end-4">
-        <img src="https://images.unsplash.com/photo-1729829471656-8488135a99ec" alt="" className="transition-all ease-in duration-150 h-full w-full object-cover md:saturate-0 md:group-hover:saturate-100" />
+type CardProps = {
+  imgUrl: string,
+  alt: string,
+  description:string
+};
+
+const Card: React.FC<CardProps> = ({ imgUrl,alt,description }) => (
+  <div className="min-h-[350px] relative max-h-[450px] group border border-gray-600 rounded-[22px] col-span-1 bg-white/10 group overflow-hidden sm:col-span-2 lg:col-span-1 last:sm:col-start-2 last:sm:col-end-4 lg:last:col-start-3 lg:last:col-end-4">
+    
+    <img
+      src={imgUrl}
+      alt={alt}
+      className="transition-all ease-in duration-150 h-full w-full object-cover lg:saturate-0 lg:group-hover:saturate-100"
+    />
+
+
+    <div className=" p-6 text-xs xl:text-sm z-1 absolute bottom-0 left-0 w-full h-max lg:translate-y-full lg:group-hover:translate-y-0 bg-black/50 text-white transition-all ease-in duration-150">
+        <p className='mb-6'>{description}</p>
+        <a href="#" className=' bg-white text-black px-4 py-2 rounded-lg'>
+            Learn More
+        </a>
     </div>
-    
-    
-    export default Card;
+
+  </div>
+);
+
+export default Card;
