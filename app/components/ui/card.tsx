5dc9d061ff59ab174b9372225d348e7fd9d117a9
Shivam Kumar <savagedeveloper795@gmail.com>
2024-10-31 15:12:19 +0530

    reduced grid size and fixed font sizing

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 };
 
 const Card: React.FC<CardProps> = ({ imgUrl,alt,description }) => (
-  <div className="min-h-[350px] relative max-h-[450px] group border border-gray-600 rounded-[22px] col-span-1 bg-white/10 group overflow-hidden sm:col-span-2 lg:col-span-1 last:sm:col-start-2 last:sm:col-end-4 lg:last:col-start-3 lg:last:col-end-4">
+  <div className="min-h-[350px] relative max-h-[450px] group border border-gray-600 rounded-[22px] col-span-1 bg-white/10 group overflow-hidden ">
     
     <img
       src={imgUrl}
@@ -16,11 +16,8 @@ const Card: React.FC<CardProps> = ({ imgUrl,alt,description }) => (
     />
 
 
-    <div className=" p-6 text-xs xl:text-sm z-1 absolute bottom-0 left-0 w-full h-max lg:translate-y-full lg:group-hover:translate-y-0 bg-black/50 text-white transition-all ease-in duration-150">
-        <p className='mb-6'>{description}</p>
-        <a href="#" className=' bg-white text-black px-4 py-2 rounded-lg'>
-            Learn More
-        </a>
+    <div className=" p-6 text-xs md:text-sm xl:text-base z-1 absolute bottom-0 left-0 w-full h-max lg:translate-y-full lg:group-hover:translate-y-0 bg-black/50 text-neutral-100 transition-all ease-in duration-150">
+        <p>{description}</p>
     </div>
 
   </div>
