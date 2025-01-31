41deb781c2a4b46796df67d810146156f5d25afe
Aditya Jyoti <aj.adityajyoti@gmail.com>
2025-02-01 00:10:28 +0530

    i think i changed something which made it correct

--- app/components/pages/homepage.tsx
+++ app/components/pages/homepage.tsx
@@ -106,6 +106,17 @@ const Homepage: React.FC = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen relative">
       <canvas
@@ -171,6 +182,7 @@ const Homepage: React.FC = () => {
           className="apply-button"
           data-hackathon-slug="hackhubvitc"
           data-button-theme="dark"
+          style={{ height: "44px", width: "312px" }}
         ></div>
       </div>
     </div>
